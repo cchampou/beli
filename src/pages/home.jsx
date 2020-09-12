6b1c418f237f91a0c05c15ba7f161307493256c5
cchampou <clement@champouillon.com>
2020-09-12 23:00:18 +0200

    feat(home): add text decoration

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,7 +8,7 @@ export default () => (
       Comment être heureux en amour ? Avec la méthode Piano !
     </Heading2>
     <Paragraph>
-      Bienvenue sur Belinda Sans Tabous ! Je suis une Love Coach et conseillère sentimentale qui vous parle sans aucuns tabous de la vie amoureuse, du célibat, de l’estime de soi et de la sexualité. Vous désirez trouver l’amour, consolider votre couple ou améliorer votre vie sexuelle ? Laissez-vous guider pour changer de vie !
+      Bienvenue sur <em>Belinda Sans Tabous</em> ! Je suis une Love Coach et conseillère sentimentale qui vous parle sans aucuns tabous de la vie amoureuse, du célibat, de l’estime de soi et de la sexualité. Vous désirez trouver l’amour, consolider votre couple ou améliorer votre vie sexuelle ? Laissez-vous guider pour changer de vie !
     </Paragraph>
   </>
 );
