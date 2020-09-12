6ed3088ec71918c574257fdd0409af6d7ffafec7
cchampou <clement@champouillon.com>
2020-09-12 22:59:42 +0200

    fix(styles): global

--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,15 @@ import { ThemeProvider } from 'emotion-theming';
 import { Global, css } from '@emotion/core';
 import { Router } from '@reach/router';
 import Home from './pages/home';
+import About from './pages/about';
 import theme from '../config/theme.json';
 import Header from './components/header';
 import Footer from './components/footer';
 
-const GlobalStyles = css`
+export const GlobalStyles = css`
   body {
     font-family: 'Open Sans', sans-serif;
+    font-weight: 300;
     margin: 0;
   }
   a {
