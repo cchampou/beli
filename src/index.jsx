a8d468f050b439ea3f8f0089956dc8c76211522c
cchampou <clement@champouillon.com>
2020-09-20 11:20:04 +0200

    With SW

--- src/index.jsx
+++ src/index.jsx
@@ -52,3 +52,13 @@ reactDom.render(<App />, document.getElementById('root'));
 if (module.hot) {
   module.hot.accept('./index', () => null);
 }
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 const commonConfig = require('./webpack.common.config');
 
 module.exports = {
   ...commonConfig,
   mode: 'production',
   entry: path.resolve('src/index.jsx'),
+  plugins: [
+    ...commonConfig.plugins,
+    new WorkboxPlugin.GenerateSW({
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
+  ],
 };
