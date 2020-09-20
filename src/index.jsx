ca941914a9a2113e38de0daebd5f0067e1cfbc3f
Clément Champouillon <clement@champouillon.com>
2020-09-20 12:59:56 +0200

    chore(ci): revert to full ci  (#14)

--- src/index.jsx
+++ src/index.jsx
@@ -55,9 +55,11 @@ if (module.hot) {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+      // eslint-disable-next-line no-console
       console.log('SW registered: ', registration);
-    }).catch(registrationError => {
+    }).catch((registrationError) => {
+      // eslint-disable-next-line no-console
       console.log('SW registration failed: ', registrationError);
     });
   });
