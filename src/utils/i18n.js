1b23a2eeb01091dba5127da3d9587d8693758e75
cchampou <clement@champouillon.com>
2020-09-20 06:45:24 +0200

    All translations ready for homepage

---
+++ src/utils/i18n.js
@@ -0,0 +1,19 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import fr from '../langs/fr.json';
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources: {
+      fr: {
+        translation: fr,
+      },
+    },
+    lng: 'fr',
+    fallbackLng: 'fr',
+
+    interpolation: {
+      escapeValue: false,
+    },
+  });
